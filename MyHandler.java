badf983502c7f52c81ce80e9afc5cd91541cbc7b
pengzzzccc <153042982+pengzzzccc@users.noreply.github.com>
2026-03-04 16:22:32 +1100

    json respone

--- MyHandler.java
+++ MyHandler.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.io.OutputStream;
 
-import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -10,23 +9,35 @@ public class MyHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange he) throws IOException {
 
-        // Handle CORS.
+        // Handle CORS
         String origin = he.getRequestHeaders().getFirst("Origin");
-        he.getResponseHeaders().add("Access-Control-Allow-Origin", origin);
+        he.getResponseHeaders().add("Access-Control-Allow-Origin", origin != null ? origin : "*");
         he.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
         he.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
 
-        // Set response headers.
-        Headers headers = he.getResponseHeaders();
-        headers.set("Content-Type", "text/plain");
-        headers.set("Connection", "close");
+        if (he.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
+            he.sendResponseHeaders(204, -1);
+            return;
+        }
 
-        // Send response body.
-        String response = String response = "{\"name\":\"Japan\",\"gold\":27,\"silver\":14,\"bronze\":17,\"
-total\":58}";
-;
-        System.out.println(response);
+        String query = he.getRequestURI().getQuery();  
+        String code = "";
+        if (query != null && query.startsWith("code=")) {
+            code = query.split("=")[1].toUpperCase();
+        }
+
+        String response;
+        switch (code) {
+            case "NOR": response = "{\"code\":\"NOR\", \"tally\":[18,12,11]}"; break;
+            case "USA": response = "{\"code\":\"USA\", \"tally\":[12,12,9]}";  break;
+            case "NED": response = "{\"code\":\"NED\", \"tally\":[10,7,2]}";   break;
+            case "ITA": response = "{\"code\":\"ITA\", \"tally\":[10,6,14]}";  break;
+            default:    response = "{\"code\":\"unknown\", \"tally\":[0,0,0]}"; break;
+        }
+
+        he.getResponseHeaders().set("Content-Type", "application/json");
         he.sendResponseHeaders(200, response.length());
+
         OutputStream os = he.getResponseBody();
         os.write(response.getBytes());
         os.close();
